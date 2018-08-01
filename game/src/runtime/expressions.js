5e50bd466f863d1e8be9d9ec660e1d6387735d09
LiTao <709540081@qq.com>
2018-08-01 10:45:04 +0800

    corona

---
+++ game/src/runtime/expressions.js
@@ -0,0 +1,51 @@
+var ls;
+(function (ls) {
+	ls.MainScene = function() {
+		return {
+			"Math.random()*25": function() { return Math.random()*25 },
+			"AISprite25": function() { return AISprite25 },
+			"System.from": function() { return System.from },
+			"%22market%22": function() { return "market" },
+			"%22lessOrEqual%22": function() { return "lessOrEqual" },
+			"bar": function() { return bar },
+			"AISprite37": function() { return AISprite37 },
+			"System.score": function() { return System.score },
+			"AISprite34": function() { return AISprite34 },
+			"%22..%2Flottery.html%22": function() { return "../lottery.html" },
+			"%22score%22": function() { return "score" },
+			"%22countdown%22": function() { return "countdown" },
+			"%22finally%22": function() { return "finally" },
+			"%22greaterOrEqual%22": function() { return "greaterOrEqual" },
+			"AISprite31": function() { return AISprite31 },
+			"AISprite19": function() { return AISprite19 },
+			"%22greaterThan%22": function() { return "greaterThan" },
+			"%22..%2Flottery.html%22": function() { return "../lottery.html" },
+			"Math.random()*414": function() { return Math.random()*414 },
+			"%22equalTo%22": function() { return "equalTo" },
+			"Math.random()": function() { return Math.random() },
+			"Math.random()*50": function() { return Math.random()*50 },
+			"AISprite28": function() { return AISprite28 },
+			"Math.random()*100": function() { return Math.random()*100 },
+			"%22var%20from%3Dwindow.location.search.split('%3D')%5B1%5D%3B%20System.from%3Dfrom%3B%22": function() { return "var from=window.location.search.split('=')[1]; System.from=from;" },
+			"System.countdown": function() { return System.countdown }
+		}
+	};
+	ls.launch = function() {
+		return {
+			"%22MainScene%22": function() { return "MainScene" }
+		}
+	};
+	ls.finally = function() {
+		return {
+			"%22..%2Flottery.html%22": function() { return "../lottery.html" },
+			"%22Corona%20Lime%20Newcomer%22": function() { return "Corona Lime Newcomer" },
+			"%22equalTo%22": function() { return "equalTo" },
+			"%22..%2Flottery.html%22": function() { return "../lottery.html" },
+			"%22Corona%20Lime%20Elite%22": function() { return "Corona Lime Elite" },
+			"%22market%22": function() { return "market" },
+			"%22bar%22": function() { return "bar" },
+			"System.score": function() { return System.score },
+			"System.from": function() { return System.from }
+		}
+	};
+})(ls || (ls = {}));
